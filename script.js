3a04d40ceefa31875f5ddb17e0dbe1245cda7247
nohid17 <.\.git\nohid.ahmed@gmail.com>
2026-01-18 15:22:19 -0500

    initialized js

---
+++ script.js
@@ -0,0 +1,45 @@
+// let age = 19;
+// let greetingText = "hi, I'm nonosha!!!!";
+// alert(greetingText);
+// greetingText = "HI, i am REALLY nonosha!!";
+// alert(greetingText);
+// alert(age);
+
+let age = 19;
+let userName = "Nonosha";
+let hobbies = ["Drawing", "Singing", "Writing", "Farting in da Cah"];
+let job = {
+  title: "Developer",
+  place: "New York",
+  salary: 50000,
+};
+
+let totalAdultYears;
+
+function calculateAdultYears() {
+  return age - 18;
+}
+
+totalAdultYears = calculateAdultYears();
+alert(totalAdultYears);
+
+age = 45;
+totalAdultYears = calculateAdultYears();
+
+alert(totalAdultYears);
+
+
+
+// alert(hobbies[3]);
+// // alert(hobbies);
+// alert("$" + job.salary + " " + job.title);
+
+// // (papas code :) /////////////////////////////////////////////////////////
+// let infoUser = { fName: "Nohid", lName: "Akhmed", age: 12 };
+
+// let fName = 'Nohid';
+// let lName = 'Akhmed';
+// // let fullName = fName + ' ' + lName;
+// let fullName = `${fName} ${lName}`;
+// // alert(fullName) 
+
